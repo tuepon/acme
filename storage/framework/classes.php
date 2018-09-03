58cf18bc8a05569406a755e1f0e6e2e8cbafdc20
Tomonori Ueno <t.uepon@gmail.com>
2018-09-03 22:53:55 +0900

    first commit

---
+++ storage/framework/classes.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'october\\demo\\Plugin' => 'plugins\\october\\demo\\Plugin.php',
+  'rainlab\\blog\\Plugin' => 'plugins\\rainlab\\blog\\Plugin.php',
+  'RainLab\\Blog\\Classes\\TagProcessor' => 'plugins\\rainlab\\blog\\classes\\TagProcessor.php',
+  'RainLab\\Blog\\Components\\Posts' => 'plugins\\rainlab\\blog\\components\\Posts.php',
+  'RainLab\\Blog\\Models\\Post' => 'plugins\\rainlab\\blog\\models\\Post.php',
+  'RainLab\\Blog\\Models\\Category' => 'plugins\\rainlab\\blog\\models\\Category.php',
+  'backend\\Controllers\\index' => 'modules\\backend\\controllers\\index.php',
+  'backend\\Controllers\\auth' => 'modules\\backend\\controllers\\auth.php',
+);
